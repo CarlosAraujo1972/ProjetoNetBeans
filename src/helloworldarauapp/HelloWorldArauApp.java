202c43577007d241a76846b85ec22687eb24b7bc
Carlos Araujo <carlos.araujo@outlook.com>
2017-11-09 19:35:16 -0200

    Commit do projeto Hello World

---
+++ src/helloworldarauapp/HelloWorldArauApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldarauapp;
+
+/**
+ *
+ * @author 3JBR2V1
+ */
+public class HelloWorldArauApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!");
+    }
+    
+}
